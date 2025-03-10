a5e2beba638cb1e42fd8214b1a6abb8289f1de6d
peas8810 <127704090+peas8810@users.noreply.github.com>
2025-03-09 22:05:24 -0300

    app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def gerar_relatorio_pdf(referencias_com_similaridade, nome, email, codigo_verifi
 
     # Adicionando os dados do usuário no PDF
     data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    pdf.chapter_title("📋 Dados do Usuário:")
+    pdf.chapter_title("Dados do Usuário:")
     pdf.chapter_body(f"Nome: {nome}")
     pdf.chapter_body(f"E-mail: {email}")
     pdf.chapter_body(f"Data e Hora: {data_hora}")
