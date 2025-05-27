8c28b9e5b6f745130b345cd3b0f9a69387dc7bf0
peas8810 <127704090+peas8810@users.noreply.github.com>
2025-05-27 18:56:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -225,8 +225,6 @@ if __name__ == "__main__":
     "</a></h3>",
     unsafe_allow_html=True
 )
-    st.components.v1.iframe("https://pay.hotmart.com/U99934745U?off=y2b5nihy&hotfeature=51&_hi=eyJjaWQiOiIxNzQ4Mjk4OTUxODE2NzQ2NTc3ODk4OTY0NzUyNTAwIiwiYmlkIjoiMTc0ODI5ODk1MTgxNjc0NjU3Nzg5ODk2NDc1MjUwMCIsInNpZCI6ImM4OTRhNDg0MzJlYzRhZTk4MTNjMDJiYWE2MzdlMjQ1In0=.1748375599003&bid=1748375601381", height=250)
-
     st.subheader("Técnica PROATIVA: Domine a Criação de Comandos Poderosos na IA e gere produtos monetizáveis  - https://peas8810.hotmart.host/product-page-1f2f7f92-949a-49f0-887c-5fa145e7c05d")
     # Exibição de imagem para propaganda (substitua a URL pela sua imagem)
     image_url = "https://static-media.hotmart.com/cu0MontuJsAjZltv6bttoE1zxbI=/filters:quality(100):format(webp)/klickart-prod/uploads/media/file/9314924/imagem_-_proativa.jpg"
